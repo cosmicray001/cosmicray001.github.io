fc341fa418d6be10a43300eb7d9bb0f82b1dbc56
cosmicray001 <cosmicray001@users.noreply.github.com>
2021-07-03 16:34:30 +0000

    deploy: cosmicray001/portefeuille@11a518d3263137f1b51283a43cbf4d98a43054f8

--- en/js/wowchemy.min.ad299157ffff5f8e3caf9b0eff697052.js
+++ en/js/wowchemy.min.ad299157ffff5f8e3caf9b0eff697052.js
@@ -1,8 +1,5 @@
 (() => {
   // <stdin>
-  /*! Wowchemy v5.1.0 | https://wowchemy.com/ */
-  /*! Copyright 2016-present George Cushen (https://georgecushen.com/) */
-  /*! License: https://github.com/wowchemy/wowchemy-hugo-modules/blob/main/LICENSE.md */
   (() => {
     var a = Object.assign || function(d2) {
       for (var a2 = 1, b2, c2; a2 < arguments.length; a2++) {
@@ -224,10 +221,16 @@
         case 0:
           localStorage.setItem("wcTheme", "0"), a2 = false, console.debug("User changed theme variation to Light.");
           break;
+          localStorage.setItem("wcTheme", "0"), a2 = false, console.debug("User changed theme variation to Light.");
+          break;
         case 1:
           localStorage.setItem("wcTheme", "1"), a2 = true, console.debug("User changed theme variation to Dark.");
           break;
+          localStorage.setItem("wcTheme", "1"), a2 = true, console.debug("User changed theme variation to Dark.");
+          break;
         default:
+          localStorage.setItem("wcTheme", "2"), window.matchMedia("(prefers-color-scheme: dark)").matches ? a2 = true : window.matchMedia("(prefers-color-scheme: light)").matches ? a2 = false : a2 = window.wc.isSiteThemeDark, console.debug("User changed theme variation to Auto.");
+          break;
           localStorage.setItem("wcTheme", "2"), window.matchMedia("(prefers-color-scheme: dark)").matches ? a2 = true : window.matchMedia("(prefers-color-scheme: light)").matches ? a2 = false : a2 = window.wc.isSiteThemeDark, console.debug("User changed theme variation to Auto.");
           break;
       }
@@ -241,10 +244,16 @@
         case 0:
           a2.classList.add("dropdown-item-active"), b2.classList.remove("dropdown-item-active"), c2.classList.remove("dropdown-item-active");
           break;
+          a2.classList.add("dropdown-item-active"), b2.classList.remove("dropdown-item-active"), c2.classList.remove("dropdown-item-active");
+          break;
         case 1:
           a2.classList.remove("dropdown-item-active"), b2.classList.add("dropdown-item-active"), c2.classList.remove("dropdown-item-active");
           break;
+          a2.classList.remove("dropdown-item-active"), b2.classList.add("dropdown-item-active"), c2.classList.remove("dropdown-item-active");
+          break;
         default:
+          a2.classList.remove("dropdown-item-active"), b2.classList.remove("dropdown-item-active"), c2.classList.add("dropdown-item-active");
+          break;
           a2.classList.remove("dropdown-item-active"), b2.classList.remove("dropdown-item-active"), c2.classList.add("dropdown-item-active");
           break;
       }
@@ -530,3 +539,6 @@
     });
   })();
 })();
+/*! Copyright 2016-present George Cushen (https://georgecushen.com/) */
+/*! License: https://github.com/wowchemy/wowchemy-hugo-modules/blob/main/LICENSE.md */
+/*! Wowchemy v5.1.0 | https://wowchemy.com/ */

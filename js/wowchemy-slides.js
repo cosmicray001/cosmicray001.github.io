758b4e7747dea857c7b3725e7a8aa4f695f44fb3
cosmicray001 <cosmicray001@users.noreply.github.com>
2021-10-25 04:37:22 +0000

    deploy: cosmicray001/portefeuille@162ef96599309f014fb784424e2a927ad9fa8bd0

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var slides = {highlight_style: "dracula", theme: "black"};
+  var slides = { highlight_style: "dracula", theme: "black" };
 
   // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy@v0.0.0-20210324194200-fda9f39d872e/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
